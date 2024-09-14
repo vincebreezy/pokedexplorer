a3e8066adbd4cf616ea30405deaf6e15134c9af8
Rodrigo Chen <rodrigo.chen@sjsu.edu>
2024-09-13 23:58:51 -0700

    Added asset folder and pokeball logo on homepage

--- frontend/our-app/src/routes/Home.js
+++ frontend/our-app/src/routes/Home.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import Navbar from "../components/Navbar"
 import Footer from "../components/Footer"
+import { NavLink } from 'react-router-dom'
+import pokeball from '../assets/pokeball.png'
+import { Link } from "react-router-dom"
 
 const Home = () => {
   return (
     <div>
-        <Navbar />
-        <Footer />
+      <Navbar />
+      <div>
+        <Link to="/pokemon">
+          <img src={pokeball} alt="pokeball" />
+        </Link>
+      </div>
+      <Footer />
     </div>
   )
 }
