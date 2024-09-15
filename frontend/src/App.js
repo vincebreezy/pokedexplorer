5e2d36c13f211865effc8491845e1fbb44e53d8e
natalie <natalie.ky.leung@gmail.com>
2024-09-15 00:45:19 -0700

    combinded yay

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,17 @@
+import { useState,useEffect } from 'react'
+import './App.css';
+import ArticleList from './routes/ArticleList'
 import React from 'react';
 import "./index.css"
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './routes/Home';
 import Pokemon from './routes/Pokemon';
-import { useState,useEffect } from 'react'
-import './App.css';
-import ArticleList from './components/ArticleList'
+import './index.css';
 
-const App = () => {
+function App() {
   const [articles, setArticles] = useState([]);
 
-   // Modify the current state by setting the new data to
+  // Modify the current state by setting the new data to
   // the response from the backend
   useEffect(()=>{
     fetch('http://localhost:5000/articles',{
@@ -26,27 +27,26 @@ const App = () => {
   },[])
 
   return (
-    <Router>
+    <div className="App container m-4">
+    {/* <div className="row">
+      <div className="text-center">
+      <h1>Connecting a React Frontend to a Flask Backend.</h1>
+      </div>
+    </div> */}
+
+      <ArticleList 
+      articles={articles} 
+      />
+      <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/pokemon" element={<Pokemon />} />
-        <div className="App container m-4">
-        <div className="row">
-          <div className="text-center">
-          <h1>Connecting a React Frontend to a Flask Backend.</h1>
-          </div>
-        </div>
-
-          <ArticleList 
-          articles={articles} 
-          />
-
-        </div>
+        <Route path="/articles" element={<ArticleList />} />
       </Routes>
     </Router>
-    
+
+    </div>
   );
-};
+}
 
 export default App;
- 
---
+++ frontend/src/NewApp.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import "./index.css"
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './routes/Home';
+import Pokemon from './routes/Pokemon';
+import './index.css';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/pokemon" element={<Pokemon />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
+ 
