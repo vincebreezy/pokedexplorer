46e3f1ab2e737ad28692ab8a3b88bd2d059e5f66
natalie <natalie.ky.leung@gmail.com>
2024-09-16 11:42:00 -0700

    added updated frontend files, new /monlist page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import './App.css';
-import ArticleList from './routes/ArticleList';
+import { EvolutionList, StatsList, MonList }  from './routes/ArticleList';
 import React from 'react';
 import './index.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
@@ -8,20 +8,42 @@ import Home from './routes/Home';
 import Pokemon from './routes/Pokemon';
 
 function App() {
-  const [articles, setArticles] = useState([]);
+  const [evolutions, setEvolutions] = useState([]);
+  const [stats, setStats] = useState([]);
+  const [monlist, setMonList] = useState([]);
 
-  // Fetch articles from backend
-  useEffect(() => {
-    fetch('http://localhost:5000/articles', {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
+  useEffect(()=>{
+    fetch('http://localhost:5000/evolutions',{
+      'methods':'GET',
+      headers : {
+        'Content-Type':'application/json'
+      }
     })
-      .then(response => response.json())
-      .then(response => setArticles(response))
-      .catch(error => console.log(error));
-  }, []);
+    .then(response => response.json())
+    .then(response => setEvolutions(response))
+    .catch(error => console.log(error))
+
+    fetch('http://localhost:5000/stats',{
+      'methods':'GET',
+      headers : {
+        'Content-Type':'application/json'
+      }
+    })
+    .then(response => response.json())
+    .then(response => setStats(response))
+    .catch(error => console.log(error))
+
+    fetch('http://localhost:5000/monlist',{
+      'methods':'GET',
+      headers : {
+        'Content-Type':'application/json'
+      }
+    })
+    .then(response => response.json())
+    .then(response => setMonList(response))
+    .catch(error => console.log(error))
+
+  },[])
 
   return (
     <div className="App container m-4">
@@ -30,7 +52,9 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/pokemon" element={<Pokemon />} />
           {/* Render ArticleList only on the /articles route */}
-          <Route path="/articles" element={<ArticleList articles={articles} />} />
+          <Route path="/evolutions" element={<EvolutionList evolutions={evolutions} />} />
+          <Route path="/stats" element={<StatsList stats={stats} />} />
+          <Route path="/monlist" element={<MonList monlist={monlist} />} />
         </Routes>
       </Router>
     </div>
--- frontend/src/routes/ArticleList.js
+++ frontend/src/routes/ArticleList.js
@@ -1,23 +1,71 @@
+const EvolutionList = (props) => {
 
-const ArticleList = (props) => {
+  return (
+      <div className="m-2">
+      {/* Display the evolution details if evolutions is not None */} 
+       {props.evolutions && props.evolutions.map(evolution =>{
+          return (
 
-    return (
-        <div className="m-2">
-        {/* Display the article details if article is not None */} 
-   	    {props.articles && props.articles.map(article =>{
-            return (
-
-              <div key= {article.id}>
-                <h2 className="text-primary"> { article.title} </h2>
-                <p> { article.body } </p>
-                <p> { article.date } </p>
-    	        <hr/>
-              </div>
-            )
-            
-            })}
-        </div>
-        )
+            <div key= {evolution.id}>
+              <h2 className="text-primary"> { evolution.name} </h2>
+              <p> Pokemon ID: { evolution.id } </p>
+              <p> Pre-Evolution: { evolution.preEvo } </p>
+              <p> Post-Evolution: { evolution.postEvo } </p>
+            <hr/>
+            </div>
+          )
+          
+          })}
+      </div>
+      )
 }
 
-export default ArticleList;
+const StatsList = (props) => {
+
+return (
+    <div className="m-2">
+    {/* Display the stat details if stats is not None */} 
+     {props.stats && props.stats.map(stat =>{
+        return (
+
+          <div key= {stat.id}>
+            <h2 className="text-primary"> Pokemon ID: { stat.id} </h2>
+            <p> HP Stat: { stat.hp } </p>
+            <p> Attack Stat: { stat.attack } </p>
+            <p> Defense Stat:{ stat.defense } </p>
+            <p> Special Attack Stat: { stat.spAtk } </p>
+            <p> Special Defense Stat: { stat.spDef } </p>
+            <p> Speed Stat: { stat.speed } </p>
+            <p> Total Base Stat: { stat.total } </p>
+          <hr/>
+          </div>
+        )
+        
+        })}
+    </div>
+    )
+}
+
+const MonList = (props) => {
+
+  return (
+      <div className="m-2">
+      {/* Display the monlist details if monlist is not None */} 
+       {props.monlist && props.monlist.map(monlist =>{
+          return (
+  
+            <div key= {monlist.id}>
+              <h2 className="text-primary"> { monlist.name} </h2>
+              <p> Pokemon ID: { monlist.id } </p>
+              <p> Type 1: { monlist.first_type } </p>
+              <p> Type 2:{ monlist.second_type } </p>
+            <hr/>
+            </div>
+          )
+          
+          })}
+      </div>
+      )
+  }
+
+export { EvolutionList, StatsList, MonList };
